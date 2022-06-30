b20c91ef93af4eb7093db461bf4be5e52442c961
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-07-01 00:01:51 +0100

    Update 📜Form.py

--- ~/app/📜Form.py
+++ ~/app/📜Form.py
@@ -10,9 +10,10 @@ image = Image.open('image_1.png')
 col1, col2 = st.columns(2)
 
 col1.header("Pink Data Hub CRM")
+col1.write("A form app with DETA.SH Database")
 col2.image(image)
 
-pink_data = st.sidebar.selectbox("choose:",("Chose","Database", "Database Connection"))
+
 
 
 deta = Deta(st.secrets["deta_key"])
